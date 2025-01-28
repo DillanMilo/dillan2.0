8819e6053ba71d901ff92472c8b10f1cf0fee2ed
DillanMilo <dillanxx@icloud.com>
2025-01-28 15:12:45 -0600

    adjust home

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -5,7 +5,7 @@ const Home: React.FC = () => {
   return (
     <div
       id="home"
-      className="relative h-screen w-screen flex items-center justify-start px-20 text-white overflow-hidden"
+      className="relative h-screen w-screen flex items-center justify-start px-5 text-white overflow-hidden"
     >
       {/* Background Image */}
       <div
@@ -15,17 +15,17 @@ const Home: React.FC = () => {
 
       {/* Content */}
       <div className="relative z-10 opacity-0 animate-fadeIn delay-[500ms]">
-        <h2 className="text-2xl text-white font-bebas opacity-0 animate-fadeIn delay-[800ms]">
+        <h2 className="text-5xl text-white font-bebas opacity-0 animate-fadeIn delay-[800ms] mb-20">
           Heya, my name is
         </h2>
 
         {/* Name Animation */}
-        <h1 className="text-[15rem] font-bebas font-bold tracking-wide transform scale-y-200 opacity-0 animate-fadeIn delay-[1100ms] flex">
+        <h1 className="text-[15rem] font-bebas font-bold tracking-wide transform scale-y-200 opacity-0 animate-fadeIn delay-[950ms] flex">
           {"Dillan".split("").map((letter, index) => (
             <span
               key={index}
               className="inline-block opacity-0 animate-slideUp"
-              style={{ animationDelay: `${index * 100 + 1200}ms` }} // Each letter appears sequentially
+              style={{ animationDelay: `${index * 100 + 1000}ms` }} // Each letter appears sequentially
             >
               {letter}
             </span>
@@ -33,13 +33,13 @@ const Home: React.FC = () => {
         </h1>
 
         {/* Description */}
-        <p className="text-lg text-gray-300 font-bebas tracking-wide opacity-0 animate-slideInLeft delay-[1800ms]">
+        <p className="text-3xl text-gray-300 font-bebas tracking-wide opacity-0 animate-slideInLeft delay-[1800ms]">
           I build stuff and make things look nice on the web.
         </p>
       </div>
 
       {/* Intro Description */}
-      <p className="absolute bottom-10 right-10 text-right text-lg text-white font-bebas max-w-md opacity-0 animate-slideInRight delay-[2500ms]">
+      <p className="absolute bottom-10 right-10 text-right text-4xl text-white font-bebas max-w-md opacity-0 animate-slideInRight delay-[2500ms]">
         I’m a creative UI and UX developer who loves crafting beautiful and
         functional websites. My passion lies in blending art with code to
         deliver engaging user experiences.
