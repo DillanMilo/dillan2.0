9171bbc8dcb80fb694df67dc5815add525e611d6
DillanMilo <dillanxx@icloud.com>
2025-01-30 13:47:18 -0600

    make info page responsive

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -45,17 +45,17 @@ const Info: React.FC = () => {
       className="relative flex flex-col items-center justify-start text-center pt-32 pb-16 px-6 overflow-hidden"
     >
       {/* Title with Expand Animation */}
-      <h1 className="text-[11rem] font-bebas uppercase tracking-wide text-white opacity-0">
+      <h1 className="text-[6rem] sm:text-[8rem] md:text-[10rem] lg:text-[11rem] font-bebas uppercase tracking-normal sm:tracking-wide max-w-[90%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-[60%] text-white opacity-0">
         Milosevich
       </h1>
 
       {/* Subtext */}
-      <p className="text-2xl text-gray-400 font-bebas mt-4 opacity-0">
+      <p className="text-xl  sm:text-2xl md:text-2xl text-gray-400 font-bebas mt-4 opacity-0">
         No, it's not a sandwich. It's my last name.
       </p>
 
       {/* Description */}
-      <p className="text-[2.5rem] text-slateGreen font-bebas max-w-[600px] opacity-0 leading-relaxed">
+      <p className="text-[1.8rem] mt-10 sm:text-[2rem] md:text-[2.5rem] text-slateGreen font-bebas max-w-[600px] opacity-0 leading-relaxed">
         Design meets functionality in my world of UI/UX development. I
         specialize in turning concepts into interactive experiences, balancing
         creativity with code to bring ideas to life. I love Nature, Food, and
