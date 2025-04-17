a368225311f20b2a0904d02742ab39a71c4ee89c
DillanMilo <dillanxx@icloud.com>
2025-04-17 12:55:02 -0500

    outline

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,25 @@ export default {
           beige: "#f5f5dc",
           slateGreen: "#a8b2ac", // Custom beige color
         },
+        textShadow: {
+          'white': '-1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff',
+        },
       },
     },
     safelist: [], // No longer needed for animations since they're in index.css
-    plugins: [],
+    plugins: [
+      function ({ addUtilities }) {
+        const newUtilities = {
+          '.text-shadow-white': {
+            'text-shadow': `-2px -2px 0 #fff, 
+                            2px -2px 0 #fff, 
+                            -2px 2px 0 #fff, 
+                            2px 2px 0 #fff,
+                            0 0 4px #fff`  // Added extra glow
+          },
+        }
+        addUtilities(newUtilities)
+      },
+    ],
   };
   
